7769f72cc7a4d70ce1737e7ce6be4e3a1b2000eb
EUSUNGKIM <jimmy0818@naver.com>
2024-08-23 18:24:45 +0900

    fix/헬기 뒷날개 버그수정
    헬기 뒷날개 회전버그수정

--- Assets/KES/Scripts/Propellers1.cs
+++ Assets/KES/Scripts/Propellers1.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Propellers : MonoBehaviour
+public class Propellers1 : MonoBehaviour
 {
     [SerializeField] float rotateSpeed;
 
     private void Update()
     {
-        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime, Space.World);
     }
 }
